971a9896773dfe486166aeac3a839b1e61d43b8f
costagavras <cgavras@yahoo.ca>
2019-12-21 18:26:05 -0500

    basic auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule) },
+  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule), canLoad: [AuthGuard] },
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then( m => m.AuthPageModule) },
-  { path: 'log', loadChildren: () => import('./log/log.module').then( m => m.LogPageModule) },
-  { path: 'training', loadChildren: () => import('./training/training.module').then( m => m.TrainingPageModule) },
-  { path: 'food', loadChildren: () => import('./food/food.module').then( m => m.FoodPageModule) },
-  { path: 'profile', loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule) }
+  { path: 'log', loadChildren: () => import('./log/log.module').then( m => m.LogPageModule), canLoad: [AuthGuard] },
+  { path: 'training', loadChildren: () => import('./training/training.module').then( m => m.TrainingPageModule), canLoad: [AuthGuard] },
+  { path: 'food', loadChildren: () => import('./food/food.module').then( m => m.FoodPageModule), canLoad: [AuthGuard] },
+  { path: 'profile', loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule), canLoad: [AuthGuard] }
 ];
 
 @NgModule({
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  // tslint:disable-next-line: variable-name
+  private _userIsAuthenticated = false;
 
-  constructor() { }
+  get userIsAuthenticated() {
+    return this._userIsAuthenticated;
+  }
+
+  constructor(private router: Router) { }
+
+  login() {
+    this._userIsAuthenticated = true;
+    this.router.navigateByUrl('/home');
+  }
+
+  logout() {
+    this._userIsAuthenticated = false;
+    this.router.navigateByUrl('/auth');
+  }
 }
