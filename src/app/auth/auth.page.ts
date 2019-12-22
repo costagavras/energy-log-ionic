682439fdc73284dea7b37d22c99574f43eba62ab
costagavras <cgavras@yahoo.ca>
2019-12-22 14:33:14 -0500

    auth form

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
 import { AuthService } from './auth.service';
 
 @Component({
@@ -7,10 +9,30 @@ import { AuthService } from './auth.service';
   styleUrls: ['./auth.page.scss'],
 })
 export class AuthPage implements OnInit {
+  isLogin = true;
 
   constructor(public authService: AuthService) { }
 
   ngOnInit() {
+  }
+
+  onSwitchAuthMode() {
+    this.isLogin = !this.isLogin;
+  }
+
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+
+    if (this.isLogin) {
+      // Send a request to login servers
+    } else {
+      // send a request to signup servers
+    }
+    form.reset();
   }
 
   onLogin() {
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
@@ -13,15 +14,20 @@ export class AuthService {
     return this._userIsAuthenticated;
   }
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private loadingController: LoadingController) { }
 
   login() {
     this.isLoading = true;
     this._userIsAuthenticated = true;
-    setTimeout(() => {
-      this.isLoading = false;
-      this.router.navigateByUrl('/home');
-    }, 500);
+    this.loadingController.create({keyboardClose: true, message: 'Logging in...'})
+      .then(loadingEl => {
+        loadingEl.present();
+        setTimeout(() => {
+          this.isLoading = false;
+          loadingEl.dismiss();
+          this.router.navigateByUrl('/home');
+        }, 500);
+      });
   }
 
   logout() {
