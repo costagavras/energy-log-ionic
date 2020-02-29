7e2da0be57e1625c5c5a54688ab45cf8a5643530
costagavras <cgavras@yahoo.ca>
2020-02-28 19:36:08 -0500

    menu and links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule), canLoad: [AuthGuard] },
+  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule) },
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then( m => m.AuthPageModule) },
   { path: 'log', loadChildren: () => import('./log/log.module').then( m => m.LogPageModule), canLoad: [AuthGuard] },
   { path: 'training', loadChildren: () => import('./training/training.module').then( m => m.TrainingPageModule), canLoad: [AuthGuard] },
